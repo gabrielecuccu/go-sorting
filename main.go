3a3143703c560fb1a1580fe270cda2dd5c64f446
gabriele <gabriele.cuccu@gmail.com>
2024-08-27 10:19:06 +0100

    added processing time for each algorithm in milliseconds

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"github.com/gabrielecuccu/go-sorting/lib"
+	"time"
 )
 
 func sort(algorithm lib.SortAlgorithm, numbers *[]int) {
 	fmt.Println("******************************************")
 	fmt.Printf("%s of %v\n", algorithm.Name(), *numbers)
+	fmt.Printf("%s\n", algorithm.Name())
+
+	start := float64(time.Now().UnixNano())
 	algorithm.Sort(numbers)
+	t := float64(time.Now().UnixNano())
+
 	fmt.Println("Result: ", *numbers)
-	fmt.Printf("In %d steps\n", algorithm.Steps())
+	fmt.Printf("In %d steps, %.3f milliseconds\n", algorithm.Steps(), (t-start)/1e6)
 	fmt.Println("******************************************")
 }
 
@@ -23,8 +29,8 @@ func unsortedArray(size int) *[]int {
 }
 
 func main() {
-	sort(lib.NewBubbleSort(), unsortedArray(25))
-	sort(lib.NewSelectionSort(), unsortedArray(25))
-	sort(lib.NewInsertionSort(), unsortedArray(25))
-	sort(lib.NewMergeSort(), unsortedArray(25))
+	sort(lib.NewBubbleSort(), unsortedArray(50))
+	sort(lib.NewSelectionSort(), unsortedArray(50))
+	sort(lib.NewInsertionSort(), unsortedArray(50))
+	sort(lib.NewMergeSort(), unsortedArray(50))
 }
